61f137fe8bb79fbab1561525888796e902fd7baf
drumfeet <drumfeet@gmail.com>
2023-10-07 07:20:30 +0800

    Header

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react"
+
+export function Header() {
+  return (
+    <Box
+      as="section"
+      pb="200px"
+      pt="88px"
+      px="344px"
+      color="#F7FAFC"
+      bg="#6B46C1"
+      textAlign="center"
+    >
+      <Heading fontSize="48px" fontWeight="800">
+        Simple pricing for your business
+      </Heading>
+      <Text fontSize="24px" fontWeight="500" pt="16px" opacity="0.8">
+        Plans that are carefully crafted to suit your business.
+      </Text>
+    </Box>
+  )
+}
---
+++ pages/pricing.js
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header"
+import { Box, Heading, Text } from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <>
+      <Box>
+        <Header />
+      </Box>
+    </>
+  )
+}
